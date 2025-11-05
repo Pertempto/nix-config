a2f7426b846d7be8428660868a3c6a8e9a423a20
Addison Emig <addison.emig@mrs-electronics.com>
2025-11-05 11:42:05 -0500

    feat: update podman config

--- modules/virtualization.nix
+++ modules/virtualization.nix
@@ -7,6 +7,8 @@
   environment.systemPackages = map lib.lowPrio [
     pkgs.qemu
     pkgs.gnome-boxes
+    pkgs.netavark
+    pkgs.aardvark-dns
   ];
 
   users.users.addison.extraGroups = [
@@ -19,6 +21,7 @@
     containers.enable = true;
     podman = {
       enable = true;
+      autoPrune.enable = true;
       dockerCompat = true;
       # Required for containers under podman-compose to be able to talk to each other.
       defaultNetwork.settings.dns_enabled = true; 
