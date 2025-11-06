dbe4b8e9d822f26bb1f6c95a32642d9e9836ed5f
Addison Emig <addison.emig@mrs-electronics.com>
2025-11-06 13:05:42 -0500

    chore: remove podman

--- dot-files/containers/README.md
+++
@@ -1,5 +0,0 @@
-# Podman Workaround
-
-We need this file for podman networking to work.
-
-See https://github.com/containers/podman/issues/22044#issuecomment-2009255504
--- modules/virtualization.nix
+++ modules/virtualization.nix
@@ -5,7 +5,6 @@
 }:
 {
   environment.systemPackages = map lib.lowPrio [
-    pkgs.podman-compose
     pkgs.qemu
     pkgs.gnome-boxes
     pkgs.slirp4netns
@@ -13,32 +12,10 @@
 
   users.users.addison.extraGroups = [
     "libvirtd"
-    "podman"
   ];
 
   virtualisation = {
     libvirtd.enable = true;
     containers.enable = true;
-    podman = {
-      enable = true;
-      autoPrune.enable = true;
-      dockerCompat = true;
-      # Required to use `/run/docker.sock`
-      dockerSocket.enable = true;
-      # Required for containers under podman-compose to be able to talk to each other.
-      defaultNetwork.settings.dns_enabled = true; 
-    };
   };
-
-  # Only use docker.io registry by default
-  environment.etc."containers/registries.conf".text = lib.mkForce ''
-    [registries.search]
-    registries = ['docker.io']
-
-    [registries.insecure]
-    registries = []
-
-    [registries.block]
-    registries = []
-  '';
 }
