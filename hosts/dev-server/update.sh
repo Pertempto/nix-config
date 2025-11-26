1f94cec9f1bd53b1b19f427ecbfd2247a3264f38
Addison Emig <addison.emig@mrs-electronics.com>
2025-11-26 16:52:20 -0500

    chore: remove extra update scripts

--- hosts/dev-server/update.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# This script is a simple way to pull the newest flake and rebuild.
-# It requires github.com/pertempto/nix-config to be cloned in ~/nix-config
-
-cd ~/nix-config
-git pull
-sudo nixos-rebuild switch --flake .#devServer
-cd -
